c41abfa26eaf17177dee8a8af5b27877c5998d10
suraiyasultana826 <suraiyasultana826@gmail.com>
2024-08-14 01:05:21 +0600

    made banner

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,40 @@
-
+'use client'
+import Image from "next/image";
+import sab from "@/assets/oo.png";
+import Typewriter from 'typewriter-effect';
+import { Box } from "@mui/material";
 const HomePage = () => {
   return (
     <div>
-      <h1>this is home page</h1>
+     <div className="hero  min-h-screen">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <Image width={600} height={800} alt='sabrinaImage' src={sab}/>
+    <div>
+    {/* <p className="py-6 text-xl">
+       This is Sabrina Islam Trina
+      </p> */}
+     
+     <h1 className="md:text-5xl text-xl font-bold">This is</h1>
+     <h1 className="md:text-5xl text-xl font-bold">
+      <Typewriter
+  options={{
+    strings: [' Sabrina Islam Trina'],
+    autoStart: true,
+    loop: true,
+  }}
+/>
+         </h1>
+  
+      <p className="py-6">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      </p>
+     <a href="https://drive.google.com/file/d/11oNqa7OiqLcb7iKTy4dg0QCKP9Sug8JF/view">
+     <button  className="btn btn-primary bg-blue-600 border-0">View My CV</button>
+     </a>
+    </div>
+  </div>
+</div>
     </div>
   );
 };
