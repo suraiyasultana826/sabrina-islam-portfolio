6930adb936b928c975136bdd73a1eb26de8b440b
suraiyasultana826 <suraiyasultana826@gmail.com>
2024-08-13 20:51:18 +0600

    made navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,156 @@
+'use client'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import Link from 'next/link';
+
+const navItems = [
+    {
+        route: "Home",
+        pathname: "/",
+    },
+    {
+        route: "About",
+        pathname: "/about",
+    },
+    {
+        route: "Education",
+        pathname: "/education",
+    },
+    {
+        route: "Experience",
+        pathname: "/experience",
+    },
+    {
+        route: "Contact ME",
+        pathname: "/",
+    },
+    
+];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function Navbar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+          className='text-black'
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Sabrina Islam
+          </Typography>
+
+          <Box  sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {navItems.map((item) => (
+                <MenuItem key={item} onClick={handleCloseNavMenu}>
+                  <Link key={item} href={item.pathname}>
+                <Button className='text-black'>{item.route}</Button>
+              </Link>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+          className='text-black'
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+           Sabrina Islam
+          </Typography>
+          <Box className='w-[70px] text-center'  sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {navItems.map((item) => (
+              <Link key={item} href={item.pathname}>
+                <Button  className='text-black '>{item.route}</Button>
+              </Link>
+            ))}
+          </Box>
+
+        
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default Navbar;
